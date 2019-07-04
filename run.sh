01958044cd834e2305fb2861f23be33deacd85bf
Tom Russell <tomalrussell@gmail.com>
2019-07-04 10:45:47 +0100

    Copy data to/from shared folder, arg to run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #
 # Run all steps
 #
+SHARED_FOLDER=$1
 
+echo $1
 
 # Dwellings
-jupyter nbconvert --execute --no-prompt arc-dwellings/convert-scenarios.ipynb
+jupyter nbconvert --execute --no-prompt ./arc-dwellings/convert-scenarios.ipynb
+cp -r ./arc-dwellings/data_processed $SHARED_FOLDER/Scenarios/Dwellings/
 
 # Economics
-jupyter nbconvert --execute --no-prompt arc-economics/convert-scenarios.ipynb
+jupyter nbconvert --execute --no-prompt ./arc-economics/convert-scenarios.ipynb
+cp -r ./arc-economics/data_processed $SHARED_FOLDER/Scenarios/Economics/
 
 # Population
 pushd ./simim
@@ -19,7 +23,7 @@ python ./scripts/postprocess.py -c ./config/camkox0he.json
 popd
 
 # Per-capita economics
-jupyter nbconvert --execute --no-prompt arc-economics/post-poulation.ipynb
+jupyter nbconvert --execute --no-prompt ./arc-economics/post-poulation.ipynb
 
 # Floor area
-jupyter nbconvert --execute --no-prompt arc-floor-area/estimate-floor-area.ipynb
+jupyter nbconvert --execute --no-prompt ./arc-floor-area/estimate-floor-area.ipynb
