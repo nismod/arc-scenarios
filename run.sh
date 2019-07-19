65720f0ac7cdb8549fd01a8443835e1999f486d9
Tom Russell <tomalrussell@gmail.com>
2019-07-19 11:04:28 +0100

    Update run.sh, with simim on accessibility branch

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #
-# Run all steps
+# Run all steps in scenarios workflow
+#
+# e.g. usage:
+#     bash -e -x run.sh ~/OneDrive\ -\ Nexus365/ARC/
 #
 SHARED_FOLDER=$1
 
@@ -7,23 +10,54 @@ echo $1
 
 # Dwellings
 jupyter nbconvert --execute --no-prompt ./arc-dwellings/convert-scenarios.ipynb
-cp -r ./arc-dwellings/data_processed $SHARED_FOLDER/Scenarios/Dwellings/
+# copy to shared folder
+cp -r ./arc-dwellings/data_processed "$SHARED_FOLDER/Scenarios/Dwellings/"
+# copy to simim
+cp ./arc-dwellings/data_processed/*.csv ./simim/data/arc/
 
 # Economics
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute --no-prompt ./arc-economics/convert-scenarios.ipynb
-cp -r ./arc-economics/data_processed $SHARED_FOLDER/Scenarios/Economics/
+# copy to shared folder
+cp -r ./arc-economics/data_processed "$SHARED_FOLDER/Scenarios/Economics/"
+# copy to simim
+cp ./arc-economics/data_processed/*.csv ./simim/data/arc/
 
 # Population
 pushd ./simim
-mkdir -p ../microsimulation/cache
-python ./scripts/run.py -c ./config/camkox0he.json
-python ./scripts/run.py -c ./config/camkox1he.json
-python ./scripts/run.py -c ./config/camkox2he.json
-python ./scripts/postprocess.py -c ./config/camkox0he.json
+    mkdir -p ./data/cache
+    python ./scripts/generate_econ_scenarios.py
+    # test
+    python ./scripts/run.py -c ./config/calibrate.json
+    python ./scripts/run.py -c ./config/gravity.json
+    python ./scripts/run.py -c ./config/production_j.json
+    python ./scripts/run.py -c ./config/test.json
+    # run gva/jobs only (zero values in households)
+    python ./scripts/run.py -c ./config/arc0-unplanned__gjh.json
+    python ./scripts/run.py -c ./config/arc1-new-cities__gjh.json
+    python ./scripts/run.py -c ./config/arc2-expansion__gjh.json
+    python ./scripts/run.py -c ./config/arc3-new-cities23__gjh.json
+    python ./scripts/run.py -c ./config/arc4-expansion23__gjh.json
+    # run sensitivities around unplanned
+    python ./scripts/run.py -c ./config/arc0-unplanned__gj-noa.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__gj.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__g-noa.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__g-a.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__gjh.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__gjx.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__h.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__j-a.json
+    python ./scripts/run.py -c ./config/arc0-unplanned__j-noa.json
+    # postprocess
+    python ./scripts/postprocess.py -c ./config/arc0-unplanned__gjh.json
 popd
 
+# copy to shared folder
+cp ./simim/data/output/arc*.csv "$SHARED_FOLDER/Scenarios/Population/data/output"
+# copy to economics
+cp ./simim/data/output/arc*.csv ./arc-economics/data_processed/
+
 # Per-capita economics
-jupyter nbconvert --execute --no-prompt ./arc-economics/post-poulation.ipynb
+jupyter nbconvert --execute --no-prompt ./arc-economics/post-population.ipynb
 
 # Floor area
 jupyter nbconvert --execute --no-prompt ./arc-floor-area/estimate-floor-area.ipynb
